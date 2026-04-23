ee8eeccd4adb12ac969a9cb27e11eae1590f328d
Adrián Povill Peña <adrian.povill@estudiant.enti.cat>
2026-04-23 22:41:23 +0200

    Upload

---
+++ MyFirstOpenGL/GameObject.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <glm.hpp>
+#include <gtc/type_ptr.hpp>
+#include <gtc/matrix_transform.hpp>
+
+class GameObject {
+
+	glm::vec3 position = glm::vec3(0.f);
+	glm::vec3 rotation = glm::vec3(0.f);
+	glm::vec3 scale = glm::vec3(1.f);
+
+};
